9f82c5cac853711351e0259ebf37cb37c2193c58
julianasalafia <juliana.salafia1@gmail.com>
2023-06-02 21:46:17 -0300

    adding the cat

--- Imma_CAT/lib/main.dart
+++ Imma_CAT/lib/main.dart
@@ -4,8 +4,30 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.indigo,
         appBar: AppBar(
-          title: Text('Imma cat'),
+          backgroundColor: Colors.indigo,
+          elevation: 0,
+          title: const Text('this is a real cat'),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Image(
+                  image: AssetImage('assets/images/i_am_cat.png'),
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Hello! \nImma real cat.',
+                style: TextStyle(
+                    fontFamily: 'Exo2', fontSize: 24, color: Colors.white),
+              ),
+            ],
+          ),
         ),
       ),
     ),
