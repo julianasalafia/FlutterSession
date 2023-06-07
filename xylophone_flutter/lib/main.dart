c765c8519ea4f4d44e805dc3cffc5e8b9f6a6434
julianasalafia <juliana.salafia1@gmail.com>
2023-06-07 10:08:44 -0300

    [XLPN-01]: Create project, add audio library, play sound

---
+++ xylophone_flutter/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: TextButton(
+              onPressed: () {
+                final player = AudioPlayer();
+                player.play(AssetSource('note1.wav'));
+              },
+              child: Text('click me'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
