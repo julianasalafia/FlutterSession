7988cd07811143cb4d2376c115f38a3af5324c4b
julianasalafia <juliana.salafia1@gmail.com>
2023-06-07 19:38:35 -0300

    restore project

---
+++ mi_card_flutter/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
