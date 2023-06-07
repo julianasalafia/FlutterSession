05c253907fe09b170d9fd33ac9c9fb33ab48aa72
julianasalafia <juliana.salafia1@gmail.com>
2023-06-07 11:28:45 -0300

    [XLPN-02]: Create colored buttons that plays sounds

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -6,21 +6,101 @@ void main() {
 }
 
 class XylophoneApp extends StatelessWidget {
-  const XylophoneApp({super.key});
+  XylophoneApp({super.key});
+
+  var audioFile1 = ('note1.wav');
+  var audioFile2 = ('note2.wav');
+  var audioFile3 = ('note3.wav');
+  var audioFile4 = ('note4.wav');
+  var audioFile5 = ('note5.wav');
+  var audioFile6 = ('note6.wav');
+  var audioFile7 = ('note7.wav');
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                player.play(AssetSource('note1.wav'));
-              },
-              child: Text('click me'),
-            ),
+          child: Column(
+            children: [
+              TextButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource(audioFile1));
+                },
+                child: Container(
+                  width: 80.0,
+                  height: 40.0,
+                  color: Colors.red,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource(audioFile2));
+                },
+                child: Container(
+                  width: 80.0,
+                  height: 40.0,
+                  color: Colors.orange,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource(audioFile3));
+                },
+                child: Container(
+                  width: 80.0,
+                  height: 40.0,
+                  color: Colors.yellow,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource(audioFile4));
+                },
+                child: Container(
+                  width: 80.0,
+                  height: 40.0,
+                  color: Colors.green,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource(audioFile5));
+                },
+                child: Container(
+                  width: 80.0,
+                  height: 40.0,
+                  color: Colors.green.shade800,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource(audioFile6));
+                },
+                child: Container(
+                  width: 80.0,
+                  height: 40.0,
+                  color: Colors.blue,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource(audioFile7));
+                },
+                child: Container(
+                  width: 80.0,
+                  height: 40.0,
+                  color: Colors.purple,
+                ),
+              ),
+            ],
           ),
         ),
       ),
