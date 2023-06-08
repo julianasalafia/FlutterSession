ba8f2e7ce75311ee1709773ac29a0fa7e8db0d7f
julianasalafia <juliana.salafia1@gmail.com>
2023-06-07 23:30:17 -0300

    [XLPN-05]: Refactor

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -5,12 +5,40 @@ void main() {
   runApp(XylophoneApp());
 }
 
+Map<String, Color> getColorMap() {
+  return {
+    'red': Colors.red,
+    'orange': Colors.orange,
+    'yellow': Colors.yellow,
+    'green': Colors.green,
+    'green.shade800': Colors.green.shade800,
+    'blue': Colors.blue,
+    'purple': Colors.purple,
+  };
+}
+
 class XylophoneApp extends StatelessWidget {
   XylophoneApp({super.key});
 
   void playSound(int soundNumber) {
     final player = AudioPlayer();
     player.play(AssetSource('note$soundNumber.wav'));
+  }
+
+  Expanded buildKey(String colorName, int soundNumber) {
+    Map<String, Color> colorMap = getColorMap();
+
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(padding: EdgeInsets.zero),
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        child: Container(
+          color: colorMap[colorName],
+        ),
+      ),
+    );
   }
 
   @override
@@ -21,83 +49,13 @@ class XylophoneApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(padding: EdgeInsets.zero),
-                  onPressed: () {
-                    playSound(1);
-                  },
-                  child: Container(
-                    color: Colors.red,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(padding: EdgeInsets.zero),
-                  onPressed: () {
-                    playSound(2);
-                  },
-                  child: Container(
-                    color: Colors.orange,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(padding: EdgeInsets.zero),
-                  onPressed: () {
-                    playSound(3);
-                  },
-                  child: Container(
-                    color: Colors.yellow,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(padding: EdgeInsets.zero),
-                  onPressed: () {
-                    playSound(4);
-                  },
-                  child: Container(
-                    color: Colors.green,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(padding: EdgeInsets.zero),
-                  onPressed: () {
-                    playSound(5);
-                  },
-                  child: Container(
-                    color: Colors.green.shade800,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(padding: EdgeInsets.zero),
-                  onPressed: () {
-                    playSound(6);
-                  },
-                  child: Container(
-                    color: Colors.blue,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(padding: EdgeInsets.zero),
-                  onPressed: () {
-                    playSound(7);
-                  },
-                  child: Container(
-                    color: Colors.purple,
-                  ),
-                ),
-              ),
+              buildKey('red', 1),
+              buildKey('orange', 2),
+              buildKey('yellow', 3),
+              buildKey('green', 4),
+              buildKey('green.shade800', 5),
+              buildKey('blue', 6),
+              buildKey('purple', 7),
             ],
           ),
         ),
