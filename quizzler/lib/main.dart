20e73666f114b12dc27a80d63d26d693b1441da2
julianasalafia <juliana.salafia1@gmail.com>
2023-06-10 12:24:25 -0300

    [QUIZZ]: Add questions and check if the user answer is right or wrong

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(Quizzler());
 
@@ -29,28 +30,15 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
   ];
+  List<bool> answers = [false, true, true];
+
+  int questionNumber = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -64,7 +52,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -81,13 +69,16 @@ class _QuizPageState extends State<QuizPage> {
               color: Colors.green,
               child: TextButton(
                 onPressed: () {
+                  bool correctAnswer = answers[questionNumber];
+
+                  if (correctAnswer == true) {
+                    print('user got it right');
+                  } else {
+                    print('user got it wrong');
+                  }
+
                   setState(() {
-                    scoreKeeper.add(
-                      Icon(
-                        Icons.check,
-                        color: Colors.green,
-                      ),
-                    );
+                    questionNumber = Random().nextInt(2);
                   });
                 },
                 child: Text(
@@ -108,7 +99,17 @@ class _QuizPageState extends State<QuizPage> {
               color: Colors.red,
               child: TextButton(
                 onPressed: () {
-                  print('false');
+                  bool correctAnswer = answers[questionNumber];
+
+                  if (correctAnswer == false) {
+                    print('user got it right');
+                  } else {
+                    print('user got it wrong');
+                  }
+
+                  setState(() {
+                    questionNumber = Random().nextInt(2);
+                  });
                 },
                 child: Text(
                   'False',
