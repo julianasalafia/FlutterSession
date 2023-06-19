95b0b5c3bfbe8b884f92ddf6b87eb69f22d6b568
julianasalafia <juliana.salafia1@gmail.com>
2023-06-19 11:13:36 -0300

    [CLIMA]: Add http package and call weather API

--- 2_AppBreweryCourse/clima/lib/screens/loading_screen.dart
+++ 2_AppBreweryCourse/clima/lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -13,6 +14,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    print('this line of code is triggered');
   }
 
   void getLocation() async {
@@ -22,8 +24,24 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getData() async {
+    http.Response response = await http.get(
+      Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=47d15a02ee7501754f83e2e344aa0ba1',
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
--- 2_AppBreweryCourse/clima/lib/services/location.dart
+++ 2_AppBreweryCourse/clima/lib/services/location.dart
@@ -1,4 +1,5 @@
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart';
 
 class Location {
   late double latitude;
