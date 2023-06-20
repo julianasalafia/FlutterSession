15e0de3baa152ec1ce93bec160c3bcbac56340a4
julianasalafia <juliana.salafia1@gmail.com>
2023-06-20 17:19:22 -0300

    [BTC]: add dropdown button

--- 2_AppBreweryCourse/bitcoin_ticker/lib/main.dart
+++ 2_AppBreweryCourse/bitcoin_ticker/lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
+      theme: ThemeData(
           primaryColor: Colors.lightBlue,
           scaffoldBackgroundColor: Colors.white),
       home: PriceScreen(),
--- 2_AppBreweryCourse/bitcoin_ticker/lib/price_screen.dart
+++ 2_AppBreweryCourse/bitcoin_ticker/lib/price_screen.dart
@@ -6,11 +6,14 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('🤑 Coin Ticker'),
+        centerTitle: true,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -42,7 +45,28 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              value: selectedCurrency,
+              items: [
+                DropdownMenuItem(
+                  child: Text('USD'),
+                  value: 'USD',
+                ),
+                DropdownMenuItem(
+                  child: Text('EUR'),
+                  value: 'EUR',
+                ),
+                DropdownMenuItem(
+                  child: Text('GBP'),
+                  value: 'GBP',
+                ),
+              ],
+              onChanged: (value) {
+                setState(() {
+                  selectedCurrency = value!;
+                });
+              },
+            ),
           ),
         ],
       ),
