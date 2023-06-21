c78c9908684abab2e4d2b9b465a26a394e37e622
julianasalafia <juliana.salafia1@gmail.com>
2023-06-21 14:27:42 -0300

    [BTC]: return btc price from API

--- 2_AppBreweryCourse/bitcoin_ticker/lib/coin_data.dart
+++ 2_AppBreweryCourse/bitcoin_ticker/lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,21 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = 'D2817716-4849-4E3D-A393-BB93E0C18BCB';
+
+class CoinData {
+  Future getCoinData() async {
+    String requestURL = '$coinAPIURL/BTC/USD?apikey=$apiKey';
+    http.Response response = await http.get(Uri.parse(requestURL));
+
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      var lastPrice = decodedData['rate'];
+      return lastPrice;
+    } else {
+      print(response.statusCode);
+      throw 'problem with the get request';
+    }
+  }
+}
--- 2_AppBreweryCourse/bitcoin_ticker/lib/price_screen.dart
+++ 2_AppBreweryCourse/bitcoin_ticker/lib/price_screen.dart
@@ -49,6 +49,24 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 
+  String bitcoinValueInUSD = '?';
+
+  void getData() async {
+    try {
+      double data = await CoinData().getCoinData();
+      setState(() {
+        bitcoinValueInUSD = data.toStringAsFixed(0);
+      });
+    } catch (exception) {
+      print(exception);
+    }
+  }
+
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -71,7 +89,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = $bitcoinValueInUSD USD',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
