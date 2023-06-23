8bd00014855a63944cf2583ac364c158f880dd4b
julianasalafia <juliana.salafia1@gmail.com>
2023-06-23 20:38:04 -0300

    [FLASHCHAT]: add AnimatedTextKit to title

--- 2_AppBreweryCourse/flash_chat/lib/screens/welcome_screen.dart
+++ 2_AppBreweryCourse/flash_chat/lib/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static String id = 'welcome_screen';
@@ -57,13 +58,18 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                     height: 60.0,
                   ),
                 ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
-                    fontSize: 45.0,
-                    color: Colors.grey.shade800,
-                    fontWeight: FontWeight.w900,
-                  ),
+                AnimatedTextKit(
+                  animatedTexts: [
+                    TypewriterAnimatedText(
+                      'Flash Chat',
+                      textStyle: TextStyle(
+                        fontSize: 45.0,
+                        color: Colors.grey.shade800,
+                        fontWeight: FontWeight.w900,
+                      ),
+                      speed: Duration(milliseconds: 120),
+                    ),
+                  ],
                 ),
               ],
             ),
