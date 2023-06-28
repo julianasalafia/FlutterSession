4ad4738a946f08cf54eefc628022f260e321cda1
julianasalafia <juliana.salafia1@gmail.com>
2023-06-27 21:47:23 -0300

    [CALCULATOR]: add functions to calculate tip, update index, etc

--- 5_Tutorials/tip_calculator/lib/main.dart
+++ 5_Tutorials/tip_calculator/lib/main.dart
@@ -13,6 +13,8 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final controller = TextEditingController();
+  List<bool> _selection = [true, false, false];
+  String? tip;
 
   @override
   Widget build(BuildContext context) {
@@ -22,16 +24,70 @@ class _MyAppState extends State<MyApp> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              if (tip != null)
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    tip!,
+                    style: TextStyle(
+                      fontSize: 30.0,
+                    ),
+                  ),
+                ),
               Text('Total Amount'),
-              TextField(
-                controller: controller,
-                textAlign: TextAlign.center,
-                decoration: InputDecoration(hintText: '\$100.00'),
+              SizedBox(
+                width: 70.0,
+                child: TextField(
+                  controller: controller,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(hintText: '\$100.00'),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ToggleButtons(
+                  children: [
+                    Text('10%'),
+                    Text('15%'),
+                    Text('20%'),
+                  ],
+                  isSelected: _selection,
+                  onPressed: updateSelection,
+                ),
+              ),
+              TextButton(
+                onPressed: calculateTip,
+                child: Text('Calculate tip'),
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.green,
+                ),
               ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  void updateSelection(int selectedIndex) {
+    setState(() {
+      for (int i = 0; i < _selection.length; i++) {
+        _selection[i] = selectedIndex == i;
+      }
+    });
+  }
+
+  void calculateTip() {
+    final totalAmount = double.parse(controller.text);
+    final selectedIndex = _selection.indexWhere((element) => element);
+    final tipPercentage = [0.1, 0.15, 0.2][selectedIndex];
+
+    final tipTotal = (totalAmount * tipPercentage).toStringAsFixed(2);
+
+    setState(() {
+      tip = '\$$tipTotal';
+    });
   }
 }
