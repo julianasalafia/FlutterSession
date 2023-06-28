a4b73eb9b0563d0071f240cde6a3cfc9e57a1d1c
julianasalafia <juliana.salafia1@gmail.com>
2023-06-28 16:17:01 -0300

    [FLASHCARD]: add flipcard

--- 5_Tutorials/flashcards_app/lib/main.dart
+++ 5_Tutorials/flashcards_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,46 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 250,
+                height: 250,
+                child: FlipCard(
+                  front: Card(
+                    elevation: 4,
+                    child: Center(
+                      child: Text('Sexy flashcard'),
+                    ),
+                  ),
+                  back: Card(
+                    elevation: 4,
+                    child: Center(
+                      child: Text('Welcome to the back side, Luke'),
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  OutlinedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.chevron_left),
+                      label: Text('Prev')),
+                  OutlinedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.chevron_right),
+                      label: Text('Next')),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
