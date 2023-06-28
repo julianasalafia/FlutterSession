2f83a6c586e6a91b8cd48d25b3c3d8693f0d2ec4
julianasalafia <juliana.salafia1@gmail.com>
2023-06-27 21:14:46 -0300

    [CALCULATOR]: create project structure

---
+++ 5_Tutorials/tip_calculator/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Total Amount'),
+              TextField(
+                controller: controller,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(hintText: '\$100.00'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
