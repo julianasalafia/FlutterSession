cf59bdcd34f1f0c1119e1111a2ce40bd7279259e
julianasalafia <juliana.salafia1@gmail.com>
2023-06-29 23:21:25 -0300

    [WEBVIEW]: add navigation buttons; add webviewcontroller; update colors

--- 5_Tutorials/show_webview/lib/main.dart
+++ 5_Tutorials/show_webview/lib/main.dart
@@ -2,32 +2,57 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp(
+    url: 'https://julianasalafia.github.io/',
+  ));
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  final String url;
+
+  MyApp({required this.url});
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  WebViewController? controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse(widget.url));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('0xFF1493'),
+          backgroundColor: Colors.black12,
+          elevation: 0,
         ),
+        body: WebViewWidget(controller: controller!),
         bottomNavigationBar: Container(
-          color: Theme.of(context).colorScheme.secondary,
+          color: Colors.black12,
           child: Padding(
             padding: EdgeInsets.only(bottom: 20.0, right: 20.0),
             child: ButtonBar(
               children: [
-                navigationButton(Icons.chevron_left, () {}),
-                navigationButton(Icons.chevron_right, () {}),
+                navigationButton(Icons.chevron_left, () async {
+                  if (await controller!.canGoBack()) {
+                    controller!.goBack();
+                  }
+                }),
+                navigationButton(Icons.chevron_right, () async {
+                  if (await controller!.canGoForward()) {
+                    controller!.goForward();
+                  }
+                }),
               ],
             ),
           ),
@@ -37,6 +62,11 @@ class _MyAppState extends State<MyApp> {
   }
 
   Widget navigationButton(IconData icon, Function() onPressed) {
-    return IconButton(icon: Icon(icon), onPressed: onPressed);
+    return IconButton(
+        icon: Icon(
+          icon,
+          color: Colors.black54,
+        ),
+        onPressed: onPressed);
   }
 }
