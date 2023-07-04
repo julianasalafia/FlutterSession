77d0f86d51f4c33011a2acfb3d15306b569d658c
julianasalafia <juliana.salafia1@gmail.com>
2023-07-03 21:11:40 -0300

    [SETTINGS]: create enums and checkboxes

--- 4_Chillin/settings/lib/main.dart
+++ 4_Chillin/settings/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'models.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -12,8 +14,63 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _usernameController = TextEditingController();
+  var _selectedGender = Gender.FEMALE;
+  var _selectedLanguages = Set<ProgrammingLanguage>();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('User Settings'),
+        ),
+        body: ListView(
+          children: [
+            ListTile(
+              title: TextField(
+                controller: _usernameController,
+                decoration: InputDecoration(labelText: 'Username'),
+              ),
+            ),
+            RadioListTile(
+                title: Text('Female'),
+                value: Gender.FEMALE,
+                groupValue: _selectedGender,
+                onChanged: (newValue) =>
+                    setState(() => _selectedGender = newValue!)),
+            RadioListTile(
+                title: Text('Male'),
+                value: Gender.MALE,
+                groupValue: _selectedGender,
+                onChanged: (newValue) =>
+                    setState(() => _selectedGender = newValue!)),
+            RadioListTile(
+                title: Text('Other'),
+                value: Gender.OTHER,
+                groupValue: _selectedGender,
+                onChanged: (newValue) =>
+                    setState(() => _selectedGender = newValue!)),
+            CheckboxListTile(
+                title: Text('Dart'),
+                value: _selectedLanguages.contains(ProgrammingLanguage.DART),
+                onChanged: (_) {}),
+            CheckboxListTile(
+                title: Text('JavaScript'),
+                value:
+                    _selectedLanguages.contains(ProgrammingLanguage.JAVASCRIPT),
+                onChanged: (_) {}),
+            CheckboxListTile(
+                title: Text('Kotlin'),
+                value: _selectedLanguages.contains(ProgrammingLanguage.KOTLIN),
+                onChanged: (_) {}),
+            CheckboxListTile(
+                title: Text('Swift'),
+                value: _selectedLanguages.contains(ProgrammingLanguage.SWIFT),
+                onChanged: (_) {}),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ 4_Chillin/settings/lib/models.dart
@@ -0,0 +1,12 @@
+enum Gender {
+  FEMALE,
+  MALE,
+  OTHER,
+}
+
+enum ProgrammingLanguage {
+  DART,
+  JAVASCRIPT,
+  KOTLIN,
+  SWIFT,
+}
