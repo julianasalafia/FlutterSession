10d69c6fc559671b544b44939dfc258d81b73389
julianasalafia <juliana.salafia1@gmail.com>
2023-07-05 09:44:08 -0300

    [REQUESTS]: create project structure

---
+++ 5_Tutorials/network_requests/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            child: Text('Make Request'),
+            onPressed: _makeRequest,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _makeRequest() async {}
+}
