117cd29666ae2e1b72225c3ff755ce1b422a39a0
julianasalafia <juliana.salafia1@gmail.com>
2023-07-08 20:08:00 -0300

    [RP]: remove unused const

--- 5_Tutorials/restaurant_picker/lib/constants.dart
+++ 5_Tutorials/restaurant_picker/lib/constants.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 const kTextColor = Color(0xFF149414);
-const kText2Color = Color(0xFF2b5329);
 
 const kColorizeTextStyle = TextStyle(
     fontSize: 18.0,
